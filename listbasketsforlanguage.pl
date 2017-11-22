b8303cce3123bc1f861333ffc00ffb40072b40aa
Adrian Fish <adrian.r.fish@gmail.com>
2017-11-22 08:03:31 +0000

    Added answers to output

--- listbasketsforlanguage.pl
+++ listbasketsforlanguage.pl
@@ -13,7 +13,9 @@ my $locale = 'eng_gb';
 my $sql = "SELECT DISTINCT basket_id FROM booklet_basket WHERE booklet_id IN (SELECT booklet_id FROM preest_assignments WHERE tl = '$locale') ORDER BY basket_id;";
 my $basket_ids = $dbh->selectcol_arrayref($sql);
 
-print "Type|Skill|Label|Prompt|Gap Text|Weight|Media Type|Text Media|File Media\n";
+my $answers_sth = $dbh->prepare("SELECT * FROM answers WHERE item_id IN (SELECT item_id FROM basket_item WHERE basket_id = ?)");
+
+print "Basket ID|Type|Skill|Label|Prompt|Gap Text|Weight|Media Type|Text Media|File Media|Answers\n";
 
 foreach my $basket_id (@$basket_ids) {
     my $basket = $dbh->selectrow_hashref("SELECT * FROM baskets WHERE id = '$basket_id'");
@@ -27,7 +29,14 @@ foreach my $basket_id (@$basket_ids) {
     my $mediatype = $basket->{'mediatype'};
     my $textmedia = $basket->{'textmedia'};
     my $filemedia = $basket->{'filemedia'};
-    print "$type|$skill|$label|$prompt|$gaptext|$weight|$mediatype|$textmedia|$filemedia\n";
+    $answers_sth->execute($basket_id);
+    my $answer_texts = '';
+    my %answers = %{$answers_sth->fetchall_hashref('id')};
+    foreach my $answer_id (keys(%answers)) {
+        my $answer = $answers{$answer_id};
+        $answer_texts .= '#' . $answer->{'text'};
+    }
+    print "$basket_id|$type|$skill|$label|$prompt|$gaptext|$weight|$mediatype|$textmedia|$filemedia|$answer_texts\n";
 }
 
 $dbh->disconnect;
